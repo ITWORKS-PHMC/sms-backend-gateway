2596c87a281569a80c22b946f095413c046f48a6
Chasey Larrisse V. Elizarde <Chasey.Elizarde@uphmc.com.ph>
2023-06-14 09:53:53 +0800

    Initial Commit

---
+++ smsBackendGateway/Controllers/SmsController.cs
@@ -0,0 +1,236 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using smsBackendGateway.Models;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NuGet.Protocol.Plugins;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+namespace smsBackendGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class SmsController : ControllerBase
+    {
+        private SerialPort serialport = new SerialPort("COM4", 115200);
+        private string[] ports = SerialPort.GetPortNames();
+
+        private readonly ILogger<SmsController> _logger;
+        private readonly SmsContext _dbContext;
+        
+        public SmsController(ILogger<SmsController> logger, SmsContext DbContext)
+        {
+            _logger = logger;
+            _dbContext = DbContext;
+            this.serialport.Parity = Parity.None;
+            this.serialport.DataBits = 8;
+            this.serialport.StopBits = StopBits.One;
+            this.serialport.Handshake = Handshake.RequestToSend;
+            this.serialport.DtrEnable = true;
+            this.serialport.RtsEnable = true;
+            this.serialport.NewLine = System.Environment.NewLine;
+        }
+
+        private List<Sms> ParseSmsMessages(string response)
+        {
+            int ctr = 0;
+            int messageId = -1;
+            string? dateAndTime = null;
+            string? message = null;
+            string? sender = null;
+
+            List<Sms> messages = new List<Sms>();
+
+            // Split the response into lines.
+            string[] lines = response.Split('\n');
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("+CMGL"))
+                {
+                    ctr = 0;
+
+                    string[] index = line.Split(',');
+
+                    messageId = int.Parse(index[0].Substring(7).ToString());
+
+                    string status = index[1].Trim('\"');
+
+                    sender = "+" + index[2].Trim('\"');
+
+                    dateAndTime = (index[4] + ", " + index[5]).Trim('\"');
+
+                    ctr++;
+                }
+                else
+                {
+                    if (ctr == 1)
+                    {
+                        message = line.ToString();
+                        messages.Add(new Sms("1", message, messageId, sender, dateAndTime));
+                        ctr = 0;
+                    }
+                }
+            }
+            return messages;
+        }
+
+        [HttpGet]
+        [Route("ReceiveMessage")]
+        public string GetAllMessages()
+        {
+            serialport.Open();
+
+            serialport.WriteLine(@"AT" + (char)(13));
+            Thread.Sleep(200);
+
+            serialport.WriteLine("AT+CMGF=1\r");
+            Thread.Sleep(200);
+
+            serialport.WriteLine("AT+CNMI=1,1,0,0\r");
+            Thread.Sleep(200);
+
+            // List ALL messages
+            serialport.WriteLine("AT+CMGL=\"ALL\"\r");
+            Thread.Sleep(10000); // 10 seconds
+
+            // Read the response from the modem.
+            string response = serialport.ReadExisting();
+
+            // Parse the response to get the list of SMS messages.
+            List<Sms> messages = ParseSmsMessages(response);
+
+            List<Sms> messageList = new List<Sms>();
+            // Display the list of SMS messages.
+            foreach (Sms message in messages)
+            {
+                //Contruct our Rows
+                messageList.Add(new Sms("1", message.message, message.messageId, message.sender, message.dateAndTime));
+                
+                try
+                {
+                    string connectionString = "Data Source=uphmc-dc33; Initial Catalog=ITWorksSMS; TrustServerCertificate=True; User ID=dalta; Password=dontshareit";
+
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    // Open the connection
+                    connection.Open();
+
+                    SqlCommand command;
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+
+                    command = connection.CreateCommand();
+
+                    String sql = "TRUNCATE TABLE contacts";
+
+                    command = new SqlCommand(sql, connection);
+                    adapter.InsertCommand = new SqlCommand(sql, connection);
+                    adapter.InsertCommand.ExecuteNonQuery();
+
+
+                    // Close the connection
+                    connection.Close();
+                }
+                catch (SqlException ex)
+                {
+                    // Handle any errors that occurred during the connection process
+                    Console.WriteLine("An error occurred while connecting to the database: " + ex.Message);
+                }
+            }
+
+            serialport.Close();
+
+            return JsonSerializer.Serialize(messageList);
+        }
+
+        private List<Sms> ParseSmsContacts(string response)
+        {
+            int messageId = -1;
+            string? date = null;
+            string? message = null;
+            string? sender = null;
+
+            List<Sms> contacts = new List<Sms>();
+
+            string[] number = response.Split(';');
+
+            foreach (string phoneNumbers in number)
+            {
+                Console.WriteLine(sender = phoneNumbers.ToString());
+
+            }
+            contacts.Add(new Sms("1", message, messageId, sender, date));
+            return contacts;
+        }
+
+        [HttpPost]
+        [Route("SendMessage")]
+        public string Send([FromBody] Sms sms)
+        {
+            serialport.Open();
+
+            serialport.WriteLine(@"AT" + (char)(13));
+            Thread.Sleep(1000);
+
+            serialport.WriteLine("AT+CMGF=1\r");
+            Thread.Sleep(1000);
+
+            serialport.WriteLine("AT+CMGS=\"" + sms.phoneNumber + "\"\r\n");
+            Thread.Sleep(1000);
+
+            serialport.WriteLine(sms.message + "\x1A");
+            Thread.Sleep(1000);
+
+            string response = serialport.ReadExisting();
+
+            List<Sms> contacts = ParseSmsContacts(response);
+
+            List<Sms> contactsList = new List<Sms>();
+            foreach (Sms contact in contacts)
+            {
+                contactsList.Add(new Sms(contact.phoneNumber));
+            }
+
+            serialport.Close();
+
+            try
+            {
+                string connectionString = "Data Source=uphmc-dc33; Initial Catalog=ITWorksSMS; TrustServerCertificate=True; User ID=dalta; Password=dontshareit";
+
+                SqlConnection connection = new SqlConnection(connectionString);
+                // Open the connection
+                connection.Open();
+
+                SqlCommand command;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                command = connection.CreateCommand();
+
+                String sql = "INSERT INTO sms_sent (contact_id, sms_message) VALUES (3, '" + sms.message + "')";
+
+                command = new SqlCommand(sql, connection);
+                adapter.InsertCommand = new SqlCommand(sql, connection);
+                adapter.InsertCommand.ExecuteNonQuery();
+
+                // Close the connection
+                connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                // Handle any errors that occurred during the connection process
+                Console.WriteLine("An error occurred while connecting to the database: " + ex.Message);
+            }
+
+            return JsonSerializer.Serialize(contactsList);
+
+        }
+
+    }
+}
---
+++ smsBackendGateway/Models/Sms.cs
@@ -0,0 +1,35 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
+
+namespace smsBackendGateway.Models
+{
+    public class Sms
+    {
+        //public int contact_id { get; set; }
+        public string phoneNumber { get; set; }
+        public string message { get; set; }
+
+        public int messageId { get; set; }
+        public string? sender { get; set; }
+        public string? dateAndTime { get; set; }
+
+        public Sms(string phoneNumber, string message, int messageId, string sender, string dateTime)
+        {
+            this.phoneNumber = phoneNumber;
+            this.message = message; 
+            this.messageId = messageId; 
+            this.sender = sender; 
+            this.dateAndTime = dateTime;
+        }
+
+        public Sms(){}
+
+        public Sms(string phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+  
+}
